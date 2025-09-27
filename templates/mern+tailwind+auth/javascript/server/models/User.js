cf8f4628cd1cdce9e533b2fefb862031072aa7d1
CHITRANSH PRASANNA <145662598+Chitransh1011@users.noreply.github.com>
2025-09-27 11:26:03 +0530

    Added user schema with password validation

--- templates/mern+tailwind+auth/javascript/server/models/User.js
+++ templates/mern+tailwind+auth/javascript/server/models/User.js
@@ -4,8 +4,19 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  },
+    password: { 
+      type: String, 
+      required: true, 
+      minlength: 8,
+      validate: {
+        validator: function (value) {
+          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&_])[A-Za-z\d@$!%*?#&_]{8,}$/.test(value);
+        },
+        message:
+          "Please enter a strong password.",
+      },
+    },
+   },
   { timestamps: true }
 );
 
