f5ed0e71b33bb74080cb42d0e4d911d038c47887
ShoaibHassan11 <soaebhasan12@users.noreply.github.com>
2025-10-02 08:35:26 +0530

    feat: Add Django stack support with command-based architecture

--- commands/scaffold.js
+++ commands/scaffold.js
@@ -1,5 +1,15 @@
 import { setupProject } from "../utils/project.js";
+import { executeStackSetup } from "./command.js";
 
 export async function createProject(projectName, config) {
-  return await setupProject(projectName, config);
+  // Check if stack supports command-based setup
+  const commandBasedStacks = ['django-react', 'django-vue', 'django-angular'];
+  
+  if (commandBasedStacks.includes(config.stack)) {
+    // New command-based approach
+    return await executeStackSetup(projectName, config);
+  } else {
+    // Legacy template-based approach
+    return await setupProject(projectName, config);
+  }
 }
